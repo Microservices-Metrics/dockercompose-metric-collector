a7f33f201ee0660cc2a834832abe7eb57f0b9a02
Bruno Romano <brunoromano@gmail.com>
2025-04-26 02:58:27 -0300

    Create response to service

--- src/main/java/com/metrics/dockercompose/controllers/CollectController.java
+++ src/main/java/com/metrics/dockercompose/controllers/CollectController.java
@@ -4,13 +4,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.FileInputStream;
+import com.metrics.dockercompose.models.CollectorResponse;
+import com.metrics.dockercompose.models.Measurement;
+import com.metrics.dockercompose.models.Metric;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -24,34 +33,61 @@ public class CollectController {
     this.resourceLoader = resourceLoader;
   }
 
-  @PostMapping()
-  public void postCollect() throws IOException {    
-    // Para facilitar a construção vou ler a partir de um arquivo local num primeiro momento
-    // depois vou trocar para ler a partir de um link de repo e verificar se tem algum arquivo chamado docker-compose.yml
-    
-    // TODO: trocar para ler a partir de um link de repo e verificar se tem algum arquivo chamado docker-compose.yml
-    Resource resource = resourceLoader.getResource("file:docker-compose-mock.yaml");
+  @PostMapping("/services")
+  public ResponseEntity<Object> postCollectServices() throws IOException {
+    // Para facilitar a construção vou ler a partir de um arquivo local num primeiro
+    // momento
+    // depois vou trocar para ler a partir de um link de repo e verificar se tem
+    // algum arquivo chamado docker-compose.yml
+
+    // TODO: trocar para ler a partir de um link de repo e verificar se tem algum
+    // arquivo chamado docker-compose.yml
+    Resource resource = resourceLoader.getResource("file:docker-compose.yaml");
     Yaml yaml = new Yaml();
+    List<String> servicesWithBuild = new ArrayList<>();
 
     try (InputStream inputStream = resource.getInputStream()) {
       String content = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-      
+
       Map<String, Object> dockerComposeData = yaml.load(content);
 
       Map<String, Object> services = (Map<String, Object>) dockerComposeData.get("services");
+
       services.forEach((serviceName, config) -> {
         Map<String, String> serviceConfig = (Map<String, String>) config;
-        System.out.println("Service Name: " + serviceName);
-        System.out.println("Build: " + serviceConfig.get("build"));
+
+        if (serviceConfig.containsKey("build")) {
+          servicesWithBuild.add(serviceName);
+        }
       });
 
-      // TODO: criar método para verificar build local
-      
+      int numServices = servicesWithBuild.size();
 
-      // TODO: criar método para verificar se tem banco de dados (a partir de enums?)
+      Measurement measurement = new Measurement(
+          "Docker compose service",
+          numServices,
+          "absolute",
+          Instant.now());
+
+      Metric metric = new Metric(
+        "Number of services",
+        "static"
+      );
+
+      CollectorResponse response = new CollectorResponse(metric, measurement);
+
+      return ResponseEntity.status(HttpStatus.OK).body(response);
     } catch (IOException e) {
-      e.printStackTrace();
-    }
+      Map<String, String> errorResponse = new HashMap<>();
 
+      errorResponse.put("error", "Failed to read file: " + e.getMessage());
+
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+  }
+
+  @PostMapping("/databases")
+  public void postCollectDatabases() {
+    // Implementar lógica para coletar informações de bancos de dados
   }
 }
